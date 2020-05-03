d575dd38a0893c096c94e521729fa691e6e39244
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2020-05-03 13:14:49 +0200

    refactor(macos-post-install): update script
    add vscode extensions install step

--- macos-post-install.sh
+++ macos-post-install.sh
@@ -5,6 +5,8 @@ BREWS=(
   'node'
   'git'
   'cask'
+  'mysql'
+  'postgresql'
 )
 
 CASKS=(
@@ -15,8 +17,8 @@ CASKS=(
   'the-unarchiver'
   'spotify'
   'slack'
-  'plex-media-server'
-  'moom'
+  'rectangle'
+  'postico'
   'microsoft-edge'
   'google-chrome'
   'free-download-manager'
@@ -24,13 +26,31 @@ CASKS=(
   'firefox'
   'docker'
   'copyclip'
-  'blisk'
 )
 
-# Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+EXTENSIONS=(
+  'apollographql.vscode-apollo'
+  'formulahendry.auto-close-tag'
+  'formulahendry.auto-rename-tag'
+  'coenraads.bracket-pair-colorizer'
+  'wesbos.theme-cobalt2'
+  'deepscan.vscode-deepscan'
+  'editorconfig.editorconfig'
+  'sleistner.vscode-fileutils'
+  'eamodio.gitlens'
+  'wix.vscode-import-cost'
+  'pkief.material-icon-theme'
+  'christian-kohler.npm-intellisense'
+  'christian-kohler.path-intellisense'
+  'esbenp.prettier-vscode'
+  'pflannery.vscode-versionlens'
+  'jpoissonnier.vscode-styled-components'
+)
+
 # Xcode Command Line Tools
 xcode-select --install
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -44,5 +64,16 @@ done
 
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
+
+cat << EOF >> ~/.zshrc
+# Add Visual Studio Code (code)
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
+
+source ~/.zshrc
+
+for extension in "${EXTENSIONS[@]}"; do
+  code --install-extension $extension
+done
 
 brew doctor
